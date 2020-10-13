42f15cb9e6742cb455ecc397578d16aa9319c7e5
RamVegiraju <rav3sa@virginia.edu>
2020-10-13 17:37:00 -0400

    Init commit

---
+++ src/main/java/CoreNLPExample.java
@@ -0,0 +1,12 @@
+import edu.stanford.nlp.coref.data.CorefChain;
+import edu.stanford.nlp.ling.*;
+import edu.stanford.nlp.ie.util.*;
+import edu.stanford.nlp.pipeline.*;
+import edu.stanford.nlp.semgraph.*;
+import edu.stanford.nlp.trees.*;
+import java.util.*;
+
+
+public class CoreNLPExample {
+
+}
